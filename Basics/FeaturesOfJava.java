189cc97ccce4c6ff25be768a5d19b89e1098fa3a
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-03 11:23:44 +0530

    add FeaturesOfJava class with detailed features

---
+++ Basics/FeaturesOfJava.java
@@ -0,0 +1,13 @@
+public class FeaturesOfJava {
+    public static void main (String[] args ){
+        System.out.println("===== Features of Java =====");
+        System.out.println("1. OOPs - Object-Oriented Programming");
+        System.out.println("2. Platform Independence");
+        System.out.println("3. Robust and Secure");
+        System.out.println("4. Multi-Threading and Concurrency");
+        System.out.println("5. Rich API and Standard Libraries");
+        System.out.println("6. Frameworks for Enterprise and Web Dev");
+        System.out.println("7. Open-Source Libraries");
+        System.out.println("8. Maintainability and Scalability");
+    }
+}
