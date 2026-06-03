4b815e0a114c8fc3c5becb0564f693610199c5c1
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-03 19:00:40 +0530

    add DetailsOfMe class with basic

---
+++ Basics/DetailsOfMe.java
@@ -0,0 +1,10 @@
+public class DetailsOfMe  {
+    public static void main(String[] args){
+        System.out.println("Name : Anurag Joshi.");
+        System.out.println("University Roll No : 123xxxx");
+        System.out.println("Course : B.tech");
+        System.out.println("Branch : Computer Science and Engineering");
+        System.out.println("College Name : Babu Banarasi Das University.");
+    }
+    
+}
