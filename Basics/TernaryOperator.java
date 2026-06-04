b9994fe25cff5db27bdc4cb23fe4a8de72b93f11
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-04 16:45:07 +0530

    Update TernaryOperator program to demonstrate conditional expressions with new logic

--- Basics/TernaryOperator.java
+++ Basics/TernaryOperator.java
@@ -1,8 +1,13 @@
 public class TernaryOperator {
     public static void main(String[] args) {
-        int a =1;
+        int a =13;
         String result = (a > 0) ? "Positive" : "Negative";
         System.out.println(result);
+
+        int b = 10;
+        String result1 = (a > b) ? "a" : "b";
+        System.out.println(result1);
+        
     }
     
 }
