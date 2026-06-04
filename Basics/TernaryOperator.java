85eb2389476cfbca4e7f013cbd3031d5b3ed6dd6
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-04 16:21:43 +0530

    Add TernaryOperator program to demonstrate conditional expressions

---
+++ Basics/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a =1;
+        String result = (a > 0) ? "Positive" : "Negative";
+        System.out.println(result);
+    }
+    
+}
