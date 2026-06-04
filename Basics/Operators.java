77daf5e036d0175eb00bb3de03a2d43a1e1c8638
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-04 08:59:37 +0530

    add Operators class to demonstrate arithmetic operations

---
+++ Basics/Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public static void main(String[] args){
+        System.out.println("Arithmetic Operators");
+        int a = 19;
+        int b = 11;
+        int sum = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        int div = a / b;
+        int mod = a % b;
+        System.out.println("Sum of a and b : " + sum);
+        System.out.println("Subtraction of a and b :" +sub);
+        System.out.println("Multiplication of a and b :" + multi);
+        System.out.println("Division of a and b :" + div);
+        System.out.println("Modulo of a and b :" + mod);
+
+    }
+}
