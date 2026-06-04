fe7e18422b80333277c87205dba725ee78c0f2fe
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-04 18:20:51 +0530

    Printing Vertical and Horizontal star.

---
+++ Basics/PatternsProblems.java
@@ -0,0 +1,13 @@
+public class PatternsProblems {
+    public static void main(String[] args) {
+        for(int i=1; i <=4; i++){
+            //  Vertical Star
+            System.out.println("*");
+        }
+        for(int i=1; i <=4; i++){
+            // Horizontal star
+            System.out.print("* ");
+        }
+    }
+    
+}
