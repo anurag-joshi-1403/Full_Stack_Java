e63bc1dde9462c0e20ac8921778270ebd7ad2622
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-05 08:33:26 +0530

    Add triangle pattern to PatternsProblems program

--- Basics/PatternsProblems.java
+++ Basics/PatternsProblems.java
@@ -8,6 +8,13 @@ public class PatternsProblems {
             // Horizontal star
             System.out.print("* ");
         }
+        System.out.println("===Triangle Pattern ===");
+        for(int i=1; i <=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
     
 }
