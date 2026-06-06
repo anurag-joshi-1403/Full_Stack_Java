bb6b7ece7209019540e468e19fd1fc311fcd6406
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-06 15:34:11 +0530

    Refactor Arrays class to improve code structure and readability

--- Basics/Arrays.java
+++ Basics/Arrays.java
@@ -1,3 +1,16 @@
 public class Arrays {
-    
+
+    public static void main(String[] args) {
+        // int array[] = new int[5];
+
+        // Array Initialization
+        int array[] = {10, 20, 30, 40, 50};
+
+        // Accessing Elements
+        System.out.println(array[0]);
+        System.out.println(array[2]);
+        System.out.println(array[1]);
+        System.out.println(array[4]);
+    }
+
 }
