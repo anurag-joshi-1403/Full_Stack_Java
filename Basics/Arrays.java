63b044793039ad8037fb73ae34e4042097246604
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-06 18:18:42 +0530

    Add array traversal example in Arrays class

--- Basics/Arrays.java
+++ Basics/Arrays.java
@@ -1,3 +1,5 @@
+
+
 public class Arrays {
 
     public static void main(String[] args) {
@@ -11,6 +13,14 @@ public class Arrays {
         System.out.println(array[2]);
         System.out.println(array[1]);
         System.out.println(array[4]);
+
+        // Array Traversal Using Loop
+        int arr[] = {11, 22, 33, 44, 55};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
     }
 
 }
