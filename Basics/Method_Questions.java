e6bfe67a5adc7358d1db1c7387c1f6ddb995f76f
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-06 12:32:06 +0530

    Add add method to Method_Questions class for summing two integers

--- Basics/Method_Questions.java
+++ Basics/Method_Questions.java
@@ -14,9 +14,17 @@ public class Method_Questions {
         return n;
     }
 
+    // Method Returning Value
+    static int add(int a, int b){
+        return a + b;
+    }
+
     public static void main(String[] args) {
         int n = 123;
         System.out.println("First digit: " + firstdigit(n));
         System.out.println("Last digit: " + lastdigit(n));
+
+        int result = add(10, 20);
+        System.out.println(result);
     }
 }
