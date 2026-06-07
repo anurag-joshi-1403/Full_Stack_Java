68f452e25b7485ed89053aa8fb68c02eb4f95a58
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-07 10:39:35 +0530

    Add functionality to calculate and display the sum of array elements

--- Basics/Arrays.java
+++ Basics/Arrays.java
@@ -1,5 +1,4 @@
 
-import java.util.*;
 
 public class Arrays {
 
@@ -24,17 +23,26 @@ public class Arrays {
             System.out.println(arr[i]);
         }
 
-        // Taking Array Input
-        System.out.println("Taking Array Input...");
-        Scanner sc = new Scanner(System.in);
-        sc.useDelimiter(",");
-        int arr2[] = new int[5];
-        for(int i =0; i < 5; i++){
-            arr2[i] = sc.nextInt();
+        // // Taking Array Input
+        // System.out.println("Taking Array Input...");
+        // Scanner sc = new Scanner(System.in);
+        // sc.useDelimiter(",");
+        // int arr2[] = new int[5];
+        // for(int i =0; i < 5; i++){
+        //     arr2[i] = sc.nextInt();
+        // }
+        // for(int i=0; i<5; i++){
+        //     System.out.println(arr2[i]);
+        // }
+
+        // Find Sum of Array Elements
+        System.out.println("Sum of Array Elements...");
+        int sum[] = {10,20,30,40,50};
+        int add = 0;
+        for(int i=0; i<arr.length; i++){
+            add += arr[i];
         }
-        for(int i=0; i<5; i++){
-            System.out.println(arr2[i]);
-        }
+        System.out.println(add);
 
     }
 
