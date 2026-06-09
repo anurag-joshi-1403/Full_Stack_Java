cf3a6981de1d70afe995244b144d7e00f9557a44
anurag-joshi-1403 <anuragjoshi0314@gmail.com>
2026-06-09 09:24:50 +0530

    Search an Element in Array

--- Basics/Arrays.java
+++ Basics/Arrays.java
@@ -1,12 +1,11 @@
 
-
 public class Arrays {
 
     public static void main(String[] args) {
         // int array[] = new int[5];
 
         // Array Initialization
-        int array[] = {10, 20, 30, 40, 50};
+        int array[] = { 10, 20, 30, 40, 50 };
 
         // Accessing Elements
         System.out.println("Accessing Elements...");
@@ -17,7 +16,7 @@ public class Arrays {
 
         // Array Traversal Using Loop
         System.out.println("Array Traversal...");
-        int arr[] = {11, 22, 33, 44, 55};
+        int arr[] = { 11, 22, 33, 44, 55 };
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -29,68 +28,81 @@ public class Arrays {
         // sc.useDelimiter(",");
         // int arr2[] = new int[5];
         // for(int i =0; i < 5; i++){
-        //     arr2[i] = sc.nextInt();
+        // arr2[i] = sc.nextInt();
         // }
         // for(int i=0; i<5; i++){
-        //     System.out.println(arr2[i]);
+        // System.out.println(arr2[i]);
         // }
 
         // Find Sum of Array Elements
         System.out.println("Sum of Array Elements...");
-        int sum[] = {10,20,30,40,50};
+        int sum[] = { 10, 20, 30, 40, 50 };
         int add = 0;
-        for(int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             add += arr[i];
         }
         System.out.println(add);
 
         // Find Largest Element
-        int l[] = {11,33,22,55,88};
+        int l[] = { 11, 33, 22, 55, 88 };
         int max = l[0];
-        for(int i = 1; i< l.length; i++){
-            if(l[i] > max){
+        for (int i = 1; i < l.length; i++) {
+            if (l[i] > max) {
                 max = l[i];
             }
         }
-        System.out.println("Largest Element: " +max);
+        System.out.println("Largest Element: " + max);
 
         // Find smallest Value
-        int s[] = {22,33,11,1,-1,3};
+        int s[] = { 22, 33, 11, 1, -1, 3 };
         int min = s[0];
-        
-        for(int i=1; i<s.length; i++){
-            if(s[i] < min){
+
+        for (int i = 1; i < s.length; i++) {
+            if (s[i] < min) {
                 min = s[i];
             }
         }
-        System.out.println("Smallest number : "+min);
-        
+        System.out.println("Smallest number : " + min);
 
         // Find Average of array elements.
-        int arr1[] = {1,2,3,4,5};
+        int arr1[] = { 1, 2, 3, 4, 5 };
         int sum1 = 0;
         System.out.println("Find Average of Array Elements...");
-        for(int i =0; i< arr1.length; i++){
+        for (int i = 0; i < arr1.length; i++) {
             sum1 = sum1 + arr1[i];
         }
-        double average = (double)sum1 / arr1.length;
+        double average = (double) sum1 / arr1.length;
         System.out.println(average);
 
         // Find Second Largest Element
-        int[] arr2 = {10,50,30,90,70};
+        int[] arr2 = { 10, 50, 30, 90, 70 };
         int largest = Integer.MIN_VALUE;
         int secondlargest = Integer.MIN_VALUE;
 
-        for(int i =0; i <arr2.length; i++){
-            if(arr2[i] > largest){
+        for (int i = 0; i < arr2.length; i++) {
+            if (arr2[i] > largest) {
                 secondlargest = largest;
                 largest = arr2[i];
-            }
-            else if(arr2[i] > secondlargest && arr[i] != largest){
-                secondlargest =arr2[i];
+            } else if (arr2[i] > secondlargest && arr[i] != largest) {
+                secondlargest = arr2[i];
             }
         }
         System.out.println("Second Largest : " + secondlargest);
+
+        // Search an Element in Array
+        int[] arr3 = { 10, 20, 30, 40, 50 };
+        int target = 30;
+        boolean found = false;
+        for (int i = 1; i < arr3.length; i++) {
+            if (arr3[i] == target) {
+                System.out.println("Target Found: " + arr3[i]);
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            System.out.println("Not Found...");
+        }
     }
 
 }
